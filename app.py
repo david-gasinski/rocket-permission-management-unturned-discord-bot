677f0ad91254488b48a1a4598c24c5800d5b0491
DavidGasinski <dgasinski1@sheffield.ac.uk>
2023-04-10 23:09:46 +0100

    init

---
+++ app.py
@@ -0,0 +1,4 @@
+# This file servers as the basis of the bot. 
+# Here the bot is ran and all the commands are loaded
+# The flask app is also ran here, hosting the permissions file
+# At first, inital setup is ran
---
+++ xml_edit.py
@@ -0,0 +1,68 @@
+import xml.etree.ElementTree as ET
+
+# XML Class
+# Loads the inital file and stores it in a variable
+# Saves the edited file
+class XML():
+    def __init__(self, file):
+        self.file = file
+        self.ref = ET.parse(file) #auto loads the file
+    
+    # Saves the file
+    def save(self, path):
+        try: 
+            self.ref.write(path)
+        except Exception as e:
+            return e
+        
+
+# Permissions Class
+# Represents indivual permissions in the XML file
+# Can be used to add, view, check and remove permissions in the file
+class Permissions():
+    def __init__(self, ref):
+        self.ref = ref
+    
+    # Iterates through the file and returns a list of members with the given permission name.
+    # If no permission ID does not match the name, it returns False
+    def retrieveMembers(self, name):
+        for permission in self.ref.ref.getroot().iter('Group'):
+            if (permission.find('Id').text.lower() == name.lower()):
+                return permission.find('Members')
+        return False
+    
+    # Returns True if given steamid is in the given permission group
+    def checkPermission(self, permission, steamid):
+        members = self.retrieveMembers(permission)
+        if (members == False):
+            return False
+        for member in members:
+            if (member.text == steamid):
+                return True
+        return False
+    
+    # Adds steamid to a given permission group
+    # If the permission group does not exist, it returns False
+    # Else returns True
+    def addPermission(self, permission, steamid):
+        # Check if steamid is already in the group
+        if (self.checkPermission(permission, steamid)):
+            return False
+        members = self.retrieveMembers(permission)
+        ET.SubElement(members, 'Member').text = steamid
+        self.ref.save(self.ref.file)
+        return True
+    
+    # Removes steamid from a give permission group
+    # If the user if not in the permssion group, returns False
+    # If the group does not exist, returns False
+    # Else returns True
+
+    
+# Example usage
+xml = XML('./files/Permissions.config.xml')
+perms = Permissions(xml)
+print(perms.checkPermission('admin', '76561198089828341')) #should be True
+print(perms.retrieveMembers('admin')[1].text) #should return a list of members
+print(perms.addPermission('admin', '76561197997040330')) #should return True
+print(perms.addPermission('admin', '76561198089828341')) #should return False
